b77d1877ef7d323a30dd22266d72540a7cfaa2e2
nmonje <nicholas.monje@gmail.com>
2011-02-26 18:46:12 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whoran::Application.config.secret_token = '413c206033a72f38bc733e4eeb5e0ef03adcc06d59edd61891e83518ebac33a7523659504a88fa18eacde755630d1eba25ce13d3bf24cf05d6ec6a5b6318430c'
